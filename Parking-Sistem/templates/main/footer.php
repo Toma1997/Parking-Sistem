7330b2de9f76332b3e742322cb90098c6f8ecc5b
Toma1997 <toma.joksimovic@gmail.com>
2018-10-27 14:30:15 +0200

    Obrisan glupi folder viska

--- Parking-Sistem/templates/main/footer.php
+++
@@ -1,7 +0,0 @@
-    </div>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="../../libs/jquery/dist/jquery.min.js"></script>
-    <script src="../../libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
