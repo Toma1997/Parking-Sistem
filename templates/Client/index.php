77fa262520c2ded7d608a8d3767f5c251a3f1d2e
Marko Milic <marko.milic997@gmail.com>
2018-10-28 18:06:09 +0100

    Pocetna strana

---
+++ templates/Client/index.php
@@ -0,0 +1,62 @@
+
+<?php require_once '../main/header.php'; ?>
+</div>
+<div class="card bg-dark text-white">
+	<img src="../../images/parking-car.jpg" class="img-fluid card-image" style="filter: blur(1px) grayscale(60%);" alt="Parking sistem auto">
+	<div class="card-img-overlay container">
+	<div class="row text-primary ">
+		<div class="col-sm-3">
+		<div class="card">		
+			<div class="card-header bg-white">Sprat 1</div>
+			<div class="card-body">
+				<h5 class="card-title">Prazna mesta</h5>
+				<p class="card-text">****</p>
+			</div>
+		</div>
+		</div>
+		<div class="col-sm-3">
+		<div class="card">
+			<div class="card-header bg-white">Sprat 2</div>
+			<div class="card-body">
+				<h5 class="card-title">Prazna mesta</h5>
+				<p class="card-text">****</p>
+			</div>
+		</div>
+		</div>
+    </div>
+	</div>
+</div>
+	
+<div class="card text-center">
+
+  <div class="card-body">
+    <h4 class="card-title">Pogodnosti parking sistema</h4>
+    <div class="row">
+	  <div class="col-sm-6">
+		<div class="card-body">
+			<p class="card-text">Jednostavna rezervacija parking mesta</p>
+		</div>
+	  </div>
+	  <div class="col-sm-6">
+		<div class="card-body">
+			<p class="card-text">Sigurnost Vašeg vozila</p>
+		</div>
+	  </div>
+	</div>
+	<div class="row">
+	  <div class="col-sm-6">
+		<div class="card-body">
+			<p class="card-text">Real-time sistem parking mesta</p>
+		</div>
+	  </div>
+	  <div class="col-sm-6">
+		<div class="card-body">
+			<p class="card-text">Lak pristup parking mestu</p>
+		</div>
+	  </div>
+	</div>
+    <a href="register.php" class="btn btn-primary">Registrujte se</a>
+  </div>
+  
+</div>
+<?php require_once '../main/footer.php'; ?>
