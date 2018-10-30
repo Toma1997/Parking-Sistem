7ff0f7975e752880d257131a77ee3893d3324191
Toma1997 <toma.joksimovic@gmail.com>
2018-10-30 02:49:52 +0100

    Dodat korisnik preko registracije

--- parking.sql
+++ parking.sql
@@ -10,59 +10,10 @@ Target Server Type    : MYSQL
 Target Server Version : 50719
 File Encoding         : 65001
 
-Date: 2018-10-29 12:24:00
+Date: 2018-10-30 02:48:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for clients
--- ----------------------------
-DROP TABLE IF EXISTS `clients`;
-CREATE TABLE `clients` (
-  `client_id` int(10) NOT NULL AUTO_INCREMENT,
-  `client_type` set('business','individual') DEFAULT '',
-  `forename` varchar(50) DEFAULT NULL,
-  `surname` varchar(60) DEFAULT NULL,
-  `telephone` varchar(20) NOT NULL,
-  `email` varchar(60) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  PRIMARY KEY (`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of clients
--- ----------------------------
-
--- ----------------------------
--- Table structure for cars
--- ----------------------------
-DROP TABLE IF EXISTS `cars`;
-CREATE TABLE `cars` (
-  `car_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `registration` varchar(25) NOT NULL,
-  `client_id` int(10) DEFAULT NULL,
-  PRIMARY KEY (`car_id`,`registration`),
-  KEY `FK_client` (`client_id`),
-  KEY `car_id` (`car_id`),
-  CONSTRAINT `FK_client` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of cars
--- ----------------------------
-
--- ----------------------------
--- Table structure for places
--- ----------------------------
-DROP TABLE IF EXISTS `places`;
-CREATE TABLE `places` (
-  `place_id` smallint(4) NOT NULL,
-  `sector` varchar(4) DEFAULT NULL,
-  `place` varchar(5) DEFAULT NULL,
-  `floor` smallint(2) DEFAULT NULL,
-  PRIMARY KEY (`place_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for appointments
@@ -84,6 +35,56 @@ CREATE TABLE `appointments` (
 -- ----------------------------
 -- Records of appointments
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for cars
+-- ----------------------------
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE `cars` (
+  `car_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `registration` varchar(25) NOT NULL,
+  `client_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`car_id`,`registration`),
+  KEY `FK_client` (`client_id`),
+  KEY `car_id` (`car_id`),
+  CONSTRAINT `FK_client` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cars
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for clients
+-- ----------------------------
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE `clients` (
+  `client_id` int(10) NOT NULL AUTO_INCREMENT,
+  `client_type` set('business','individual') DEFAULT '',
+  `forename` varchar(50) DEFAULT NULL,
+  `surname` varchar(60) DEFAULT NULL,
+  `telephone` varchar(20) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of clients
+-- ----------------------------
+INSERT INTO `clients` VALUES ('1', 'individual', 'Toma', 'Joksimovic', '063-334-774', 'toma.joksimovic@gmail.com', '0906fabb6dba5f470b1d4b164c1ccb446dc34835');
+
+-- ----------------------------
+-- Table structure for places
+-- ----------------------------
+DROP TABLE IF EXISTS `places`;
+CREATE TABLE `places` (
+  `place_id` smallint(4) NOT NULL,
+  `sector` varchar(4) DEFAULT NULL,
+  `place` varchar(5) DEFAULT NULL,
+  `floor` smallint(2) DEFAULT NULL,
+  PRIMARY KEY (`place_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of places
