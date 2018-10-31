8cf34d3cd3ccce81e188c509e22262ea7f83a3af
Toma1997 <toma.joksimovic@gmail.com>
2018-10-31 02:38:25 +0100

    Sredjena provera za login

--- templates/Client/login.php
+++ templates/Client/login.php
@@ -16,6 +16,39 @@
                     Prijavite se
                 </button>
             </div>
+            <div class="form-group">
+            <?php
+
+                if ($_POST) {
+                    extract($_POST);
+                    $greske = array();
+
+                    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                        $greske[] = "<h5>Email nije validan !</h5>";
+                    }
+                    
+                    if(!preg_match("/^^(?=[^\d]*\d)(?=[A-Z\d ]*[^A-Z\d ]).{8,}$/", $password)){
+                        $greske[] = "<h5>Lozinka nije validna !</h5>";
+                    }
+
+                    if(count($greske)){
+                        $greske = implode("", $greske);
+                        echo $greske;
+                    } else{
+
+                        include("./kernel/database_wrapper.php");
+                        $db = new Database("parking");
+                        $db->proveriKorisnika($_POST);
+
+                        if($db->getResult()){
+                            header("Location: ./index.php?stranica=");
+                        }
+                    }
+
+                }
+
+            ?>
+            </div>
         </form>
     </div>
 </div>
