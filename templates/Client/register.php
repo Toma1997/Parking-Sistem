780f4cfcb5c2ac1bb3beb9d94592a351260b249b
Toma1997 <toma.joksimovic@gmail.com>
2018-11-02 02:16:34 +0100

    Resen problem lozinke i captcha, greska oko header sa sesijom iz captcha

--- templates/Client/register.php
+++ templates/Client/register.php
@@ -2,7 +2,6 @@
 <div>
     <h1>Registracija korisnika</h1>
     <div class="content">
-        <!-- < ?php if (!$Context->get('hideForm')): ?>-->
             <form method="post">
 				<div class="form-group">
                     <label for="client_type">Tip lica:</label>
@@ -16,45 +15,28 @@
 					</div>
                 </div>
                 <div class="form-group">
-					<!--<label class="font-weight-light for="forename">Ime:</label>-->
-                    <input type="text" id="forename" name="forename" class="form-control" value="<?php echo $forename ?? '';?>" required placeholder="Unesite ime ">
+                    <input type="text" id="forename" name="forename" class="form-control" value="<?php echo $_POST['forename'] ?? '';?>" required placeholder="Unesite ime ">
                 </div>
                 <div class="form-group">
-                    <!--<label class="font-weight-light for="surname">Prezime:</label>-->
-                    <input type="text" id="surname" name="surname" class="form-control" value="<?php echo $surname ?? '';?>" required placeholder="Unesite prezime">
+                    <input type="text" id="surname" name="surname" class="form-control" value="<?php echo $_POST['surname'] ?? '';?>" required placeholder="Unesite prezime">
                 </div>
                 <div class="form-group">
-                    <!--<label for="email">Adresa e-pošte:</label>-->
-                    <input type="email" id="email" name="email" class="form-control" value="<?php echo $email ?? '';?>"  required placeholder="Unesite adresu e-pošte">					
+                    <input type="email" id="email" name="email" class="form-control" value="<?php echo $_POST['email'] ?? '';?>"  required placeholder="Unesite adresu e-pošte">					
                 </div>
                 <div class="form-group">
 					<label class="font-weight-light" for="telephone">Broj telefona mora da ima šablon: 063-111-888(8)</label>
-                    <!--<label for="telephone">Broj telefona:</label>-->
-                    <input type="text" id="telephone" name="telephone" class="form-control" value="<?php echo $telephone ?? '';?>" required placeholder="Unesite broj telefona">
+                    <input type="text" id="telephone" name="telephone" class="form-control" value="<?php echo $_POST['telephone'] ?? '';?>" required placeholder="Unesite broj telefona">
                 </div>
-				<!-- Proveri da li nam treba bolje, mozda samo kao brojac
-				<div class="form-group">
-                    <label for="car">Broj vozila:</label>
-					<input type="number" id="car" name="car"  class="form-control" required min="1" max="10" value="1">
-                </div>-->
-				<!--<div class="form-group">
-					<label for="registration">Registracija:</label>
-					<textarea class="form-control" rows="5" maxlength="9" id="registration" name="registration " required placeholder="Unesite broj ili brojeve registarske tablice"></textarea>
-					<label class="font-weight-light" for="telephone">Unesite jednu ili više registarskih tablica šablona: BG111SD. Svaku u posebnom redu</label>
-				</div>	-->
 				<div class="form-group">
 					<label class="font-weight-light" for="telephone">Lozinka mora imati minimum: 8 karaktera od toga jedno malo slovo, jedno veliko, specijalni znak i broj.</label>
-                    <!--<label for="password1">Lozinka:</label>-->
                     <input type="password" id="password1" name="password1" class="form-control" required placeholder="Unesite lozinku">
                 </div>
                 <div class="form-group">
-                    <!--<label for="password2">Ponovite lozinku:</label>-->
                     <input type="password" id="password2" name="password2" class="form-control" required placeholder="Unesite ponovo lozinku">
                 </div>
 				<div class="form-group">
 					<img id="captcha" class="col-sm-4 img-fluid" src="securimage/securimage_show.php" alt="CAPTCHA Image" />
-					<input type="text" id="captcha_code" name="captcha_code" class="form-control" size="10" placeholder="Unesite kod sa slike" maxlength="6" required />
-					
+					<input type="text" id="captcha_code" name="code" class="form-control" size="10" placeholder="Unesite kod sa slike" maxlength="6" required />			
 				</div>
 				
                 <div class="form-group">
@@ -66,51 +48,42 @@
 
             <div class="form-group">
             <?php
-
                 if ($_POST) {
                     extract($_POST);
                     $greske = array();
-            
+                
                     if(!preg_match("/^[A-Z][a-zA-Z']+$/", $forename)){
                         $greske[] = "<h5>Ime nije validno !</h5>";
                     }
-            
+                
                     if(!preg_match("/^[A-Z][a-zA-Z']+$/", $surname)){
                         $greske[] = "<h5>Prezime nije validno !</h5>";
                     }
-            
+                
                     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
                         $greske[] = "<h5>Email nije validan !</h5>";
                     }
-            
+                
                     if(!preg_match("/^06[0-9]\-[0-9]{3,4}\-[0-9]{3,4}$/", $telephone)){
                         $greske[] = "<h5>Telefon nije validan !</h5>";
                     }
                     
-                    if(!preg_match("/^^(?=[^\d]*\d)(?=[A-Z\d ]*[^A-Z\d ]).{8,}$/", $password1)){
+                    if(!preg_match("/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,}$/", $password1)){
                         $greske[] = "<h5>Lozinka nije validna !</h5>";
                     } else {
                         if($password1 !== $password2){
-                            $greske[] = "<h6>Nije uspesno potvrdjena lozinka !</h6>";
+                            $greske[] = "<h5>Nije uspesno potvrdjena lozinka !</h5>";
                         }
                     }
-					
-					//CAPTCHA VALIDATION 
-					// NECE ZBOG :
-					/*Warning: Cannot modify header information - headers already sent by (output started at C:\xampp\htdocs\Parking-Sistem2\templates\client\register.php:33) 
-					in C:\xampp\htdocs\Parking-Sistem2\templates\client\register.php on line 118*/
+                
+                    //CAPTCHA validacija 
+                    include_once $_SERVER['DOCUMENT_ROOT'] . '/Parking-Sistem/securimage/securimage.php';
+                    $securimage = new Securimage();
+                                    
+                    if (isset($_POST['code']) && !$securimage->check((string)$_POST['code'])) {
+                        $greske[] = "<h5>Uneseni kod se ne poklapa sa onim sa slike !</h5>";
+                    }
 
-					/*include_once $_SERVER['DOCUMENT_ROOT'] . '/Parking-Sistem/securimage/securimage.php';
-					$securimage = new Securimage();
-					
-					if ($securimage->check($_POST['captcha_code']) == false) {
-					  // the code was incorrect
-					  // you should handle the error so that the form processor doesn't continue
-
-					  // or you can use the following code if there is no validation or you do not know how
-					  $greske[] =  "The security code entered was incorrect.<br /><br />";
-					  exit;
-					}*/
                     if(count($greske)){
                         $greske = implode("", $greske);
                         echo $greske;
@@ -118,22 +91,28 @@
 
                         include("./kernel/database_wrapper.php");
                         $db = new Database("parking");
-                        $db->dodajKorisnika($_POST);
+                        $db->Connect();
+                        $db->proveriKorisnika("email", "telephone", $_POST['email'], $_POST['telephone']);
 
                         if($db->getResult()){
-                            header("Location: ../index.php?stranica=login");
-                        }
-                    }
+                            echo "<h3>Vec ste registrovani !</h3>";
+                            //header("Location: ../index.php?stranica=login"); ne radi nesto zbog sesije pogledacu naknadno
+                        } else {
+                            $db->dodajKorisnika($_POST);
 
+                            if(!$db->getResult()){
+                                $db->__destruct();
+                                echo "<h3> Greska pri validaciji forme !</h3>";
+                                
+                            } else{
+                                header("Location: ../index.php?stranica=login");
+                            }
+                        }                        
+                    }
                 }
 
             ?>
             </div>
-        <!--< ?php endif; ?>
-        <p>
-            < ?php echo htmlspecialchars($Context->get('message')); ?>
-        </p>-->
     </div>
 </div>
-<!-- Proveriti za oninvalid ne radi kako treba
- oninvalid="this.setCustomValidity('Popunite polje')"-->
+
