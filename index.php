08ecb3d24ea5d09f78c108f861a9eadcf47cf3df
Toma1997 <toma.joksimovic@gmail.com>
2018-11-02 02:13:17 +0100

    Neka mala izmena i testiranje

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
 $stranica = $_GET['stranica'] ?? '';
-session_start();
-
 
 if ($stranica == 'logout') {
 	unset($_SESSION['CLIENT']);
@@ -13,27 +11,27 @@ require_once 'templates/main/header.php';
 
 switch ($stranica) {
 	case '' :
-		include('templates/client/home.php');
+		include('templates/Client/home.php');
 		break;
 		
 	case 'register' :
 		korisnik();
-		include('templates/client/register.php');
+		include('templates/Client/register.php');
 		break;
 		
 	case 'login' :
 		korisnik();
-		include('templates/client/login.php');
+		include('templates/Client/login.php');
 		break;
 		
 	case 'parking' :
 		anonimac();
-		include('templates/client/parking.php');
+		include('templates/Client/parking.php');
 		break;
 		
 	case 'reserve' :
 		anonimac();
-		include('templates/client/reserve.php');
+		include('templates/Client/reserve.php');
 		break;
 	default:
 		echo '<div class="card-body"> <h4 class="card-title">Greška 404! Nemam takvu stranicu.</h4></div>';
